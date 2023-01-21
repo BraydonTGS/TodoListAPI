3045fcede6e6b8597d412b04c70c2456754193cd
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-01-21 08:06:18 -0600

    AlabasterTodo.DataAccess
    Created Todo and User Models, Initialized my first db migration. Stashed the Connection string

---
+++ AlabasterTodo/AlabasterTodo.DataAccess/Models/AlabasterTodoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AlabasterTodo.DataAccess.Models
+{
+    public class AlabasterTodoDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasOne<User>(u => u.User).WithMany(t => t.TodoItems).HasForeignKey(u => u.UserId);
+        }
+
+        private string GetConnectionString()
+        {
+            string c = Directory.GetCurrentDirectory();
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(c).AddJsonFile("appsettings.json").Build();
+            string? connectionStringIs = configuration.GetConnectionString("AlabasterTodo");
+            if (connectionStringIs != null)
+            {
+                return connectionStringIs;
+            }
+            throw new ArgumentNullException(nameof(connectionStringIs));
+        }
+    }
+}
+
---
+++ AlabasterTodo/AlabasterTodo.DataAccess/Models/TodoItem.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AlabasterTodo.DataAccess.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public DateTime DateCompleted { get; set; }
+
+        [Required, NotNull]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public bool IsDeleted { get; set; }
+        [Required]
+        public bool IsCompleted { get; set; }
+        
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
